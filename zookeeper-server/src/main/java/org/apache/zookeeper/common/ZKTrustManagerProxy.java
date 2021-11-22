798501d2d938f32125c61b9a1af60de7eb7b48c0
Dipesh Dutta <dipesh.dutta@oracle.com>
2021-11-23 00:43:07 +0530

    Add comments

--- zookeeper-server/src/main/java/org/apache/zookeeper/common/ZKTrustManagerProxy.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/common/ZKTrustManagerProxy.java
@@ -6,14 +6,28 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+/**
+ * This is the ZKTrustManager proxy class.
+ * This is used to expose ZKTrustManager class through proxy instance to avoid class cast error
+ * as X509TrustManagerImpl will conflict with ZKTrustManager class
+ */
 public class ZKTrustManagerProxy implements InvocationHandler {
     private static final Logger LOG = LoggerFactory.getLogger(ZKTrustManagerProxy.class);
-    private ZKTrustManager zkTrustManager;
+    private final ZKTrustManager zkTrustManager;
 
     public ZKTrustManagerProxy(ZKTrustManager zkTrustManager) {
         this.zkTrustManager = zkTrustManager;
     }
 
+    /**
+     * Invoke method implementation of proxy instance
+     *
+     * @param proxy  proxy object
+     * @param method method object
+     * @param args   method arguments
+     * @return return the output which comes from invoke mrthod
+     * @throws Throwable it throws any kind of exception which pop up in executing the given method
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         LOG.debug("Invoking method {} with arguments {}", method.getName(), args);
